5764f86c8a97a02c49792d6921c9d2439ab724c5
huoquanli <lihuoquan@gmail.com>
2020-06-14 16:26:25 +0800

    update

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"gin-demo/route"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
 func init() {
 	fmt.Println("init conf!!!")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+	}).Info("A walrus appears")
 	gin.SetMode("debug")
 }
 
